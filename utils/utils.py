cb740930a90bbbfc128990185dc68e14e25cd20c
oleg.lilitko <oleg.lilitko@gmail.com>
2019-03-13 01:30:06 +0700

    added sex_formatter

--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,35 @@
-def kb_formatter(kb):
+import math
+
+
+def kb_formatter(kb, precision=2):
     kb = int(kb)
     if kb < 1024:
-        return kb.__str__() + "KB"
+        return round(kb, precision).__str__() + "KB"
     elif kb < 1048576:
-        return (kb/1024).__str__() + "MB"
+        return round(kb / 1024, precision).__str__() + "MB"
     elif kb < 1073741824:
-        return (kb / 1048576).__str__() + "GB"
+        return round(kb / 1048576, precision).__str__() + "GB"
     else:
-        return (kb / 1073741824).__str__() + "TB"
+        return round(kb / 1073741824, precision).__str__() + "TB"
+
+
+"""
+converts seconds to days, hours, minutes, seconds
+"""
+
+
+def sex_formatter(seconds):
+    m, s = divmod(float(seconds), 60)
+    h, m = divmod(m, 60)
+    d, h = divmod(h, 24)
+    result = ""
+    if d != 0:
+        result += int(d).__str__() + " days, "
+    if h != 0:
+        result += int(h).__str__() + " hours, "
+    if m != 0:
+        result += int(m).__str__() + " minutes, "
+    if s != 0:
+        result += round(s, 3).__str__() + " seconds, "
+
+    return result
